2b6e92ea7b5381773c20d361ba93fcdde29532c5
Ivy <i.kiryakova1@newcastle.ac.uk>
2019-11-05 13:45:42 +0000

    Modifies all pictures in spec2-images

--- specification-2/spec2.py
+++ specification-2/spec2.py
@@ -1,22 +1,14 @@
+import os
 from PIL import Image
-import os, sys
 
-pic = "../resources/img/spec2-images/image01.jpg"
+pic = "../resources/img/spec2-images"
 thumb = (512, 512)
-modf = "../resources/img/spec2-modif"
 
-im = Image.open(pic)
+for i in os.listdir(pic):
+    file, ext = os.path.splitext(i)
+    im = Image.open(pic + "/" + i)
+    im.convert('RGB')
+    im.thumbnail(thumb)
+    im.save(i + "_thumbnail" + ".JPEG")
+    im.close()
 
-print(im.format, im.size, im.mode)
-
-im.show()
-
-im.convert('RGB')
-
-im.thumbnail(thumb, Image.ANTIALIAS)
-
-im.save(pic + ".thumbnail", "JPEG")
-
-print(os.stat(modf).st_size)
-
-im.show()
