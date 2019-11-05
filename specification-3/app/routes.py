111e8df4525062a296e24f216b15b13595bc8a3c
b9031685 <b9031685@newcastle.ac.uk>
2019-11-05 14:09:02 +0000

    getting page links to work

--- specification-3/app/routes.py
+++ specification-3/app/routes.py
@@ -4,9 +4,13 @@ from flask import render_template
 @app.route('/')
 @app.route('/index')
 def index():
-    user={'username': 'John'}
     lines=[
         {'author':'Oscar', 'content':'This is Team 27'},
         {'author':'John', 'content':'This is Team 34'}
     ]
-    return render_template('index.html', user=user, lines=lines)
+    return render_template('index.html', lines=lines)
+
+@app.route('/other')
+def other():
+    return render_template('other.html')
+
