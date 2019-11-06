a894e0a1fb353ba560071045f3715be259a11956
Ivy <i.kiryakova1@newcastle.ac.uk>
2019-11-06 17:14:38 +0000

    Added a command line interface

--- specification-2/spec2.py
+++ specification-2/spec2.py
@@ -4,11 +4,18 @@ from PIL import Image
 pic = "../resources/img/spec2-images"
 thumb = (512, 512)
 
-for i in os.listdir(pic):
-    file, ext = os.path.splitext(i)
-    im = Image.open(pic + "/" + i)
-    im.convert('RGB')
-    im.thumbnail(thumb)
-    im.save(file + "_thumbnail" + ".JPEG")
-    im.close()
+def create_thumbnail():
+    for i in os.listdir(pic):
+        file, ext = os.path.splitext(i)
+        im = Image.open(pic + "/" + i)
+        im.convert('RGB')
+        im.thumbnail(thumb)
+        im.save(file + "_thumbnail" + ".JPEG")
+        im.close()
 
+ask_user = input("What would you like to do?")
+if ask_user == "thumb":
+    print("Creating thumbnails...")
+    create_thumbnail()
+else:
+    print("Invalid command")
