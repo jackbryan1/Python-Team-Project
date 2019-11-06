09c65d77952f28939a7bd1158a7f24ba737ad326
Ivy <i.kiryakova1@newcastle.ac.uk>
2019-11-06 18:09:29 +0000

    Added emboss, contour and smoothen feature

--- specification-2/spec2.py
+++ specification-2/spec2.py
@@ -1,5 +1,7 @@
 import os
 from PIL import Image
+from PIL import ImageFilter
+
 
 pic = "../resources/img/spec2-images"
 thumb = (512, 512)
@@ -13,9 +15,47 @@ def create_thumbnail():
         im.save(file + "_thumbnail" + ".JPEG")
         im.close()
 
+def apply_emboss():
+    for i in os.listdir(pic):
+        file, ext = os.path.splitext(i)
+        im = Image.open(pic + "/" + i)
+        im.filter(ImageFilter.EMBOSS)
+        im.save(file + "_emboss" + ".JPEG")
+        im.close()
+
+def apply_contour():
+    for i in os.listdir(pic):
+        file, ext = os.path.splitext(i)
+        im = Image.open(pic + "/" + i)
+        im.filter(ImageFilter.CONTOUR)
+        im.save(file + "_contour" + ".JPEG")
+        im.close()
+
+def smoothen():
+    for i in os.listdir(pic):
+        file, ext = os.path.splitext(i)
+        im = Image.open(pic + "/" + i)
+        im.filter(ImageFilter.SMOOTH_MORE)
+        im.save(file + "_smooth" + ".JPEG")
+        im.close()
+
 ask_user = input("What would you like to do?")
-if ask_user == "thumb":
+
+if ask_user == "thumbnail":
     print("Creating thumbnails...")
     create_thumbnail()
+
+elif ask_user == "emboss":
+    print("Applying emboss...")
+    apply_emboss()
+
+elif ask_user == "contour":
+    print("Applying contour...")
+    apply_contour()
+
+elif ask_user == "smooth":
+    print("Smoothening...")
+    smoothen()
+
 else:
     print("Invalid command")
