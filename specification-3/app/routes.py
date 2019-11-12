a75bc08ad01ae08ed23795910effcdba73f0f113
b9031685 <b9031685@newcastle.ac.uk>
2019-11-12 10:53:05 +0000

    making README files open in reading mode (cannot be changed)

--- specification-3/app/routes.py
+++ specification-3/app/routes.py
@@ -12,12 +12,12 @@ def home():
 def spec1():
     '''render specification 1, parse in code, information and specification number'''
     files = glob.glob("./specification-1/*/*/*.py") + glob.glob("./specification-1/*/*.py") + glob.glob(
-        "./specification-1/*.py")
-    f = open("specification-1/README.md")
+        "./specification-1/*.py") #search for python files in directory up to 3 levels
+    f = open("specification-1/README.md", 'r')
     spec_info = f.readlines()
     length = len(files)
     lines = []
-    for i in range(length):
+    for i in range(length): #for each file in directory
         f = open(files[i], 'r')
         lines.append(f.readlines())
     return render_template('spec.html', lines=lines, spec_info=spec_info, spec=1, files=files)
@@ -26,12 +26,12 @@ def spec1():
 def spec2():
     '''render specification 2, parse in code, information and specification number'''
     files = glob.glob("./specification-2/*/*/*.py") + glob.glob("./specification-2/*/*.py") + glob.glob(
-        "./specification-2/*.py")
-    f = open("specification-2/README.md")
+        "./specification-2/*.py") #search for python files in directory up to 3 levels
+    f = open("specification-2/README.md", 'r')
     spec_info = f.readlines()
     length = len(files)
     lines = []
-    for i in range(length):
+    for i in range(length): #for each file in directory
         f = open(files[i], 'r')
         lines.append(f.readlines())
     return render_template('spec.html', spec_info=spec_info, lines=lines, spec=2, files=files)
@@ -40,12 +40,12 @@ def spec2():
 def spec3():
     '''render specification 3, parse in code, information and specification number'''
     files = glob.glob("./specification-3/*/*/*.html") + glob.glob("./specification-3/*/*.py") + glob.glob(
-        "./specification-3/*.py")
-    f = open("specification-3/README.md")
+        "./specification-3/*.py") #search for python files in directory up to 2 levels, html in third
+    f = open("specification-3/README.md", 'r')
     spec_info = f.readlines()
     length = len(files)
     lines = []
-    for i in range(length):
+    for i in range(length): #for each file in directory
         f = open(files[i], 'r')
         lines.append(f.readlines())
     return render_template('spec.html', lines=lines, spec_info=spec_info, spec=3, files=files)
@@ -54,12 +54,12 @@ def spec3():
 def spec4():
     '''render specification 4, parse in code, information and specification number'''
     files = glob.glob("./specification-4/*/*/*.py") + glob.glob("./specification-4/*/*.py") + glob.glob(
-        "./specification-4/*.py")
-    f = open("specification-4/README.md")
+        "./specification-4/*.py") #search for python files in directory up to 3 levels
+    f = open("specification-4/README.md", 'r')
     spec_info = f.readlines()
     length = len(files)
     lines = []
-    for i in range(length):
+    for i in range(length): #for each file in directory
         f = open(files[i], 'r')
         lines.append(f.readlines())
     return render_template('spec.html', lines=lines, spec_info=spec_info, spec=4, files=files)
