a0bd1a8bf712129590c8c92e5185e5e09057bc29
b9015402 <b9015402@newcastle.ac.uk>
2019-11-13 09:59:05 +0000

    can write hash to file

--- specification-4/hash.py
+++ specification-4/hash.py
@@ -5,8 +5,6 @@ def hashing(args):
     salt = bcrypt.gensalt()
     hashed = bcrypt.hashpw(args.password.encode('utf8'),salt)
     file1 = open('specification-4/file1.txt', 'wb')
-    print('h')
     file1.write(hashed)
-    print('h')
     file1.close()
 
